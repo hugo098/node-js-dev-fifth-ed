adc00cb851620e6f3221a0b9af714c3935c75952
hugo098 <hi.rodriguez1998@gmail.com>
2021-06-27 13:48:01 -0400

    Chapter 13

---
+++ chapter-13/asserts/test-deleteFile.mjs
@@ -0,0 +1,7 @@
+import assert from 'assert';
+import { deleteFile } from './deleteFile.mjs';
+deleteFile("no-such-file", (err) => {
+    assert.ok(err);
+    assert.ok(err instanceof Error);
+    assert.match(err.message, /does not exist/);
+});
